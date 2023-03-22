43f0005f33934fd239f45e9808b62a8200edadb8
Andrey Mamona <andrey.jbjv@gmail.com>
2023-03-22 16:19:02 +0300

    gitignore and settings.py updated

--- todo/settings.py
+++ todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x+xawzkkoqxrafw%&jy_8jej24amcb*p%p$af9@^urpuc=v@e4'
+SECRET_KEY = os.getenv(MY_SECRET)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
